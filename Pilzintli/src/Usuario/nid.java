eec5b241736a10e67334019f7c2edd7033456e26
owenPato <91210094+owenPato@users.noreply.github.com>
2022-03-30 09:27:37 -0600

    nivelactu

---
+++ Pilzintli/src/Usuario/nid.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuario;
+
+import ConexionDB.DbConnection;
+
+/**
+ *
+ * @author Owen Axel H
+ */
+public class nid {
+    DbConnection conex = new DbConnection();
+    public void InsertarN()
+}
