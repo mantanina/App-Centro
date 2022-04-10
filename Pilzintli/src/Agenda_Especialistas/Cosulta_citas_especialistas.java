5177b886fd28dcacc6b3dba4e9e7730f798c9e6c
QuiterioSalvador <100863125+QuiterioSalvador@users.noreply.github.com>
2022-04-10 11:17:12 -0500

    Avance Consultas Especialista
    Hace la consulta por la fecha seleccionada

--- Pilzintli/src/Agenda_Especialistas/Cosulta_citas_especialistas.java
+++ Pilzintli/src/Agenda_Especialistas/Cosulta_citas_especialistas.java
@@ -29,16 +29,23 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
      */
     public Cosulta_citas_especialistas() throws SQLException {
         initComponents();
+        combo();
+    }
+
+    public void buscaFecha() throws SQLException {
         DefaultTableModel modelo = new DefaultTableModel();
         tabla.setModel(modelo);
+
+        Date fecha = agenda_Calendario.getDate();
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        String fech = formato.format(fecha);
 
         PreparedStatement ps = null;
         ResultSet rs = null;
         DbConnection conn = new DbConnection();
         java.sql.Connection con = conn.getConnection();
-
-        String combo = "SELECT especialista_id";
-        String sql = "SELECT fecha,hora,paciente_id,especialista_id,tipo_consulta_id,terapia_id FROM consulta";
+        String sql = "SELECT fecha,hora,paciente_id,especialista_id,tipo_consulta_id,terapia_id FROM consulta WHERE fecha='" + fech + "'";// where fecha="+fech;
+        System.out.println(sql);
         ps = (PreparedStatement) con.prepareStatement(sql);
         rs = ps.executeQuery();
 
@@ -49,17 +56,16 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
         modelo.addColumn("Paciente");
         modelo.addColumn("Especialista");
         modelo.addColumn("Tipo de Consulta");
-        modelo.addColumn("Terap");
+        modelo.addColumn("Terapia");
+
         while (rs.next()) {
             Object[] filas = new Object[cantidadColumnas];
             for (int i = 0; i < cantidadColumnas; i++) {
                 filas[i] = rs.getObject(i + 1);
             }
-
             modelo.addRow(filas);
         }
 
-        combo();
     }
 
     public void combo() throws SQLException {
@@ -69,7 +75,7 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
         DbConnection conn = new DbConnection();
         java.sql.Connection con = conn.getConnection();
 
-        String combo = "SELECT especialista_id FROM consulta";
+        String combo = "SELECT DISTINCTROW nombre FROM especialista";
         ps = (PreparedStatement) con.prepareStatement(combo);
         rs = ps.executeQuery();
 
@@ -81,7 +87,7 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
             for (int i = 0; i < cantidadColumnas; i++) {
                 filas[i] = rs.getObject(i + 1);
             }
-            cmb_especialistas.addItem(Arrays.deepToString(filas)+"");
+            cmb_especialistas.addItem(Arrays.toString(filas) + "");
         }
     }
 
@@ -94,16 +100,17 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jCalendar1 = new com.toedter.calendar.JCalendar();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         cmb_especialistas = new javax.swing.JComboBox<>();
         btn_Buscar = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        agenda_Calendario = new com.toedter.calendar.JDateChooser();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        agenda_Calendario = new com.toedter.calendar.JDateChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -127,20 +134,20 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
 
         tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null},
-                {null, null},
-                {null, null},
-                {null, null}
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
             },
             new String [] {
-                "Fecha", "Hora"
+                "Fecha", "Hora", "Paciente", "Especialista", "Tipo De Consulta", "Terapia"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false
+                false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -162,43 +169,44 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 360, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
                                 .addComponent(btn_Buscar)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton2))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(agenda_Calendario, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jLabel1)
-                                .addGap(18, 18, 18)
-                                .addComponent(cmb_especialistas, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jButton2))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(355, 355, 355)
+                        .addComponent(jLabel2)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(420, Short.MAX_VALUE)
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(cmb_especialistas, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(agenda_Calendario, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(356, 356, 356)
-                .addComponent(jLabel2)
-                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(38, 38, 38)
                 .addComponent(jLabel2)
                 .addGap(68, 68, 68)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(agenda_Calendario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(cmb_especialistas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel1)))
-                .addGap(18, 51, Short.MAX_VALUE)
+                        .addComponent(jLabel3)
+                        .addComponent(jLabel1))
+                    .addComponent(agenda_Calendario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -211,20 +219,26 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btn_BuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_BuscarActionPerformed
-        Date fecha = agenda_Calendario.getDate();
-        SimpleDateFormat formato = new SimpleDateFormat("d/MM/yyyy");
-        JOptionPane.showMessageDialog(null, "La fecha es: " + formato.format(fecha));
+        try {
+            buscaFecha();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }//GEN-LAST:event_btn_BuscarActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -275,6 +289,7 @@ public class Cosulta_citas_especialistas extends javax.swing.JFrame {
     private javax.swing.JButton btn_Buscar;
     private javax.swing.JComboBox<String> cmb_especialistas;
     private javax.swing.JButton jButton2;
+    private com.toedter.calendar.JCalendar jCalendar1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
